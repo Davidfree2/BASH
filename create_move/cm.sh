a0abe6813426405e13a277491ad2bd1bd0ecaf4f
David Esquerra <davidesquerra.de@gmail.com>
2022-05-03 10:34:35 -0700

    added createmove which is a quick and dirty way of doing things

---
+++ create_move/cm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this file must either be sourced using source in the command line before running bash or
+#you must add a . before running this file
+#for example use . ./cm.sh or source cm.sh
+
+
+
+mkdir $1 
+cd $1
+
